6d5b25dd96fb152e627d9944544131355159532d
Sagardevil <sagarafar01@gmail.com>
2026-02-06 10:56:24 +0530

    Updated the port

--- server.js
+++ server.js
@@ -2,14 +2,15 @@ const express=require('express');
 const usersdata=require('./MOCK_DATA.json')
 const app=express();
 
-const port=3000;
+
 app.use(express.json());
 app.use(express.urlencoded({extended:true}));
 
 app.get('/api/users',(req,res)=>{
     res.json(usersdata);
 });
+const port = process.env.PORT || 3000;
 
-app.listen(port,()=>{
-    console.log(`Server is running on http://localhost:${port}`);
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
 });
